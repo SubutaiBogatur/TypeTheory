2a505c1cb2367c0dd76058d2357df4aaef70a04e
Aleksandr Tukallo <alex.tukallo@gmail.com>
2017-02-24 17:35:51 +0300

    Major: alpha equivalence reworked
    Now parser supports new grammar rules and variable names can be much longer than only one symbol, now third and fourth homeworks are totally done./LambdaParser.out

--- OCaml/LambdaParser/LambdaParser.ml
+++ OCaml/LambdaParser/LambdaParser.ml
@@ -13,7 +13,7 @@ let string_of_lambda l =
 			| App (x, y) -> s ^ "(" ^ (impl x "") ^ (impl y "") ^ ")" in
 	impl l "";;
 
-(* Let variable name be 1 symbol *)
+(* Let variable have name of grammar [a...z]{[0...9]}* *)
 (* string -> lambda *)
 let lambda_of_string s =
 	let s = s ^ ";" in
@@ -25,9 +25,16 @@ let lambda_of_string s =
 		
 	(* unit -> string *)
 	let parse_ident_str () =
+		let rec digits_adder s =
+			if (get ()) >= '0' && (get ()) <= '9' 
+				then 
+					let c = get() in
+					next();
+					digits_adder (s ^ String.make 1 c)
+				else s in
 		let var = String.make 1 (get ()) in
-		next();
-		var in 	
+		next ();
+		digits_adder var in	
 
 	(* unit -> lambda *)
 	let parse_ident () = 
@@ -63,28 +70,40 @@ let lambda_of_string s =
 
 	parse_lambda ();;
 
-(* Function substitutes v_new in all !free! occurences of v_old in lambda *)
-(* lambda -> string -> string -> lambda *)
-let rec subst l v_old v_new =
-	match l with 
-		Var v -> if v = v_old then Var v_new else Var v
-		| Abs (v, x) -> 
-			(* If v_old is enslaved by lambda, it's not free, no more subst *)
-			if v = v_old then l else Abs (v, subst x v_old v_new)
-		| App (x, y) ->
-			App (subst x v_old v_new, subst y v_old v_new);;
 	
 
 (* lambda -> lambda -> bool *)
-let rec is_alpha_equivalent x y =
-	match (x, y) with
-		(Var a, Var b) -> a = b (* Check if same vars *)
-		| (App (p, q), App(r, s)) ->
-			(is_alpha_equivalent p r) && (is_alpha_equivalent q s)	
-		| (Abs (v1, p), Abs (v2, q)) ->
-			let v = "t" in (* Todo function to generate them *)
-			is_alpha_equivalent (subst p v1 v) (subst q v2 v)
-		| _ -> false;; 
+let is_alpha_equivalent x y =
+	let subst_counter = ref 0 in
+	
+	(* Function generates next name for substitution *)	
+	(* unit -> string *)
+	let next_name_generator () =
+		let res = "t" ^ string_of_int !subst_counter in
+		subst_counter := !subst_counter + 1; 
+		res in
+
+	(* Function substitutes v_new in all !free! occurences of v_old in lambda *)
+	(* lambda -> string -> string -> lambda *)
+	let rec subst l v_old v_new =
+		match l with 
+			Var v -> if v = v_old then Var v_new else Var v
+			| Abs (v, x) -> 
+				(* If v_old is enslaved by lambda, no more subst *)
+				if v = v_old then l else Abs (v, subst x v_old v_new)
+			| App (x, y) ->
+				App (subst x v_old v_new, subst y v_old v_new) in
+
+	let rec impl x y =
+		match (x, y) with
+			(Var a, Var b) -> a = b (* Check if same vars *)
+			| (App (p, q), App(r, s)) ->
+				(impl p r) && (impl q s)	
+			| (Abs (v1, p), Abs (v2, q)) ->
+				let v = (next_name_generator ()) in
+				impl (subst p v1 v) (subst q v2 v)
+			| _ -> false in
+	impl x y;; 
 
 
 
--- OCaml/LambdaParser/tests.ml
+++ OCaml/LambdaParser/tests.ml
@@ -1,14 +1,5 @@
 open LambdaParser;;
 
-(* Tests for parser *)
-print_string (string_of_lambda (lambda_of_string "\\x.\\y.xy")); print_string "\n";;
-print_string (string_of_lambda (lambda_of_string "xy")); print_string "\n";;
-print_string (string_of_lambda (lambda_of_string "(x)")); print_string "\n";;
-print_string (string_of_lambda (lambda_of_string "(((((((\\y.y)))))))")); print_string "\n";;
-print_string (string_of_lambda (lambda_of_string "((z))(\\x.\\y.((xy)))")); print_string "\n";;
-print_string (string_of_lambda (lambda_of_string "\\l.\\i.\\f.\\e.(l)(i)(f)(esgood)")); print_string "\n";;
-print_string (string_of_lambda (lambda_of_string "\\x.\\y.xy")); print_string "\n";;
-
 let fst t =
 	let (x, y) = t in
 	x;;
@@ -17,14 +8,37 @@ let snd t =
 	let (x, y) = t in
 	y;;
 
+(* Tests for parser *)
+(* print_string (string_of_lambda (lambda_of_string "\\x.\\y.xy")); print_string "\n";;
+print_string (string_of_lambda (lambda_of_string "xy")); print_string "\n";;
+print_string (string_of_lambda (lambda_of_string "(x)")); print_string "\n";;
+print_string (string_of_lambda (lambda_of_string "(((((((\\y.y)))))))")); print_string "\n";;
+print_string (string_of_lambda (lambda_of_string "((z))(\\x.\\y.((xy)))")); print_string "\n";;
+print_string (string_of_lambda (lambda_of_string "\\l.\\i.\\f.\\e.(l)(i)(f)(esgood)")); print_string "\n";;
+print_string (string_of_lambda (lambda_of_string "\\x.\\y.xy")); print_string "\n";;
+print_string (string_of_lambda (lambda_of_string "x1")); print_string "\n";; *)
+
 (* Tests for is_alpha_equivalent function *)
-let t1 = (lambda_of_string "(x)", lambda_of_string "(x)");;
+
+let tester_eq tup =
+	print_string 
+		(string_of_bool
+			(is_alpha_equivalent
+				(fst tup)
+				(snd tup))); print_string "\n";;
+
+let t1 = (lambda_of_string "(x)", lambda_of_string "(y)");;
 let t2 = (lambda_of_string "xy", lambda_of_string "xy");;
 let t3 = (lambda_of_string "\\x.xy", lambda_of_string "\\y.yy");;
 let t4 = (lambda_of_string "\\x.x", lambda_of_string "\\y.y");;
-let t5 = (lambda_of_string "(\\x.x)", lambda_of_string "(\\y.y)");;
-print_string (string_of_bool (is_alpha_equivalent (fst t1) (snd t1))); print_string "\n";; 
-print_string (string_of_bool (is_alpha_equivalent (fst t2) (snd t2))); print_string "\n";; 
-print_string (string_of_bool (is_alpha_equivalent (fst t3) (snd t3))); print_string "\n";; 
-print_string (string_of_bool (is_alpha_equivalent (fst t4) (snd t4))); print_string "\n";; 
-print_string (string_of_bool (is_alpha_equivalent (fst t5) (snd t5))); print_string "\n";; 
+let t5 = (lambda_of_string "(\\x.x)(z)(w)", lambda_of_string "(\\y.y)(z)(w)");;
+let t6 = (lambda_of_string "\\x1.\\x2.\\x3.\\x4.x1x2x3x4", lambda_of_string "\\y1.\\y2.\\y3.\\y4.y1y2y3y4");;
+let t7 = (lambda_of_string "\\x1.\\x2.\\x3.\\x4.x4x2x3x1", lambda_of_string "(\\y.y)(z)(w)");;
+
+tester_eq t1;
+tester_eq t2;
+tester_eq t3;
+tester_eq t4;
+tester_eq t5;
+tester_eq t6;
+tester_eq t7;
