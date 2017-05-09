0590396164e98fbb8313979f05b6550efd51b19a
Aleksandr Tukallo <alex.tukallo@gmail.com>
2017-05-09 15:07:04 +0300

    Major: huge steps in implementing Robinson are done
    Idea for implementation created, but today no time to code more. Will continue in a week

--- OCaml/Robinson/hw2_unify.ml
+++ OCaml/Robinson/hw2_unify.ml
@@ -20,6 +20,10 @@ let print_equation eq str =
 let print_eq sys = 
 	print_equation sys "";; 
 
+let ps sys =
+	print_string (system_to_string sys "");
+	print_string "\n";;
+
 
 (* ------------------------ System to Equation ------------------------------ *)
 
@@ -87,6 +91,7 @@ print_string "\n";;
 
 (* ------------------------------ Apply substitution ------------------------------*)
 
+(* todo change strucutre *)
 module StringMap = Map.Make (String);;
 
 let rec apply_helper_list y map = 
@@ -107,6 +112,13 @@ let apply_substitution subst at =
 
 	apply_helper at (fill_map subst StringMap.empty);;
 
+let subst_system subst sys =
+	let rec impl subst sys acc =
+		match sys with
+			[] -> List.rev acc
+			| ((l, r)::t) -> impl subst t (((apply_substitution subst l), (apply_substitution subst r))::acc) in
+	impl subst sys [];;
+
 
 (* Test samples *)
 let at0 = Fun("f",[Fun("g",[Var "y"; Var "x"])]);;
@@ -118,11 +130,86 @@ print_string (type_to_string (apply_substitution ["y", Var "Alex"] at0) "");;
 
 (* ------------------------------ Check solution -------------------------------*)
 
+(* Function checks if two ats are equal by structure *)
+(* at -> at -> bool *)
+let rec eq_at at1 at2 =
+	match (at1, at2) with
+		(Var a, Var b) -> a = b
+		| (Fun(f, l1), Fun(g, l2)) -> f = g && eq_list l1 l2 
+		| _ -> false
+
+and eq_list l1 l2 =
+	match (l1, l2) with
+		([], []) -> true
+		| (h1::t1), (h2::t2) -> (eq_at h1 h2) && (eq_list t1 t2) 
+		| _ -> false;;
+
+(* Sorry for such names *)
+let eq_eq eq = 
+	match eq with
+		(lhs, rhs) -> eq_at lhs rhs;;
+
+
+let check_solution solution sys =
+	
+	let eq_eq_after_subst eq = 
+		match eq with
+			(lhs, rhs) -> eq_at (apply_substitution solution lhs) (apply_substitution solution rhs) in
+	
+	let rec impl sys =
+		match sys with
+			[] -> true
+			| (h::t) -> (eq_eq_after_subst h) && (impl t) in
+	impl sys;;
+
+(* Test samples *)
+
+let e1 = [(Var "a", Var "b"); (Var "x", Var "b")];;
+print_string (string_of_bool (check_solution ["a", Var "b"; "x", Var "b"] e1));;
+
+
+
+(* ------------------- Robinson algorithm: here it is -------------------------- *)
+
+(* todo mb move inside *)
+exception NoSolution of string;;
+
+module StringSet = Set.Make (String);;
+
+let solve_system sys =
+	
+	(* Checks if var is present in at *)
+	let rec contains var at = 
+		match at with
+			(Var a) -> a = var
+			| (Fun (f, l)) -> contains_l var l
+	and contains_l var l =
+		match l with
+			[] -> false
+			| (h::t) -> (contains var h) || (contains_l var t) in
+
+	(* Function solves the system. It gets system and set of resolved vars *)
+	(* list of pairs of at -> Set of strings -> list of pairs of at *)
+	let rec impl sys resolved =
+		if StringSet.cardinal resolved = List.length sys then sys else 	
+		match sys with
+			[] -> raise (NoSolution "Empty system")
+			| (cur::tail) ->
+				let lhs, rhs = cur in 	
+				(* If right side is the same as left one just 
+					remove the equation from the system *)
+				if eq_at lhs rhs then impl tail resolved else
+				match (lhs, rhs) with 
+					(Var a, any) -> if contains a any then raise (NoSolution "Third rule abused") 
+							else let resolved = StringSet.add a resolved in
+							impl (List.append (subst_system [a, any] tail) [cur]) resolved 
+								
+
+	sys;;	
 
 
 
 
-let check_solution x y = failwith "Not implemented";;
 
 
 
@@ -139,6 +226,11 @@ let check_solution x y = failwith "Not implemented";;
 
 
 
-let solve_system x = failwith "Not implemented";;
+
+
+
+
+
+
 
 
